838449fbb0541fc3d3a342b0147bb18e23685343
jhenriquea <61152747+jhenriquea@users.noreply.github.com>
2021-02-25 18:35:21 +0000

    COISO

--- app/src/main/java/com/example/test/FirstFragment.kt
+++ app/src/main/java/com/example/test/FirstFragment.kt
@@ -20,7 +20,7 @@ class FirstFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
-
+    //COISO
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
